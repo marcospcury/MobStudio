05d50e6e3497641f898cb13a6b9e1cf2c863902c
Marcos Cury <marcospcury@gmail.com>
2017-09-25 21:54:11 -0300

    auto commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var git = require('gulp-git');
 
 gulp.task('publish', function(){
-    return gulp.src('./*')
+    return gulp.src('.')
         .pipe(git.add())
         .pipe(git.commit('auto commit'))
         .pipe(git.push('umbler', 'master', function(err){if(err) throw err;}));
