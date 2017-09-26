923674f7082646a8963c3f4227e42053dadbd794
Marcos Cury <marcospcury@gmail.com>
2017-09-25 21:50:47 -0300

    client libs

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var git = require('gulp-git');
+
+gulp.task('publish', function(){
+    return gulp.src('./*')
+        .pipe(git.add())
+        .pipe(git.commit('auto commit'))
+        .pipe(git.push('umbler', 'master', function(err){if(err) throw err;}));
+});
+
